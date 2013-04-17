819b27ee2534a3bbb078c7d249a64e657b1eaf6b
Ganesh <ganesh@ganesh-Inspiron-N4050.(none)>
2013-04-17 14:39:48 +0545

    adding files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DynamicDropdown::Application.config.secret_token = 'df18e9ced3a534e0f8f4069387cd66d725fc87485fc4693dbd99ba7445d952595bdab0c377b222ce5fb87d93ccc0db0c48619d7d94328e4f6ffc07d6a823dc25'
